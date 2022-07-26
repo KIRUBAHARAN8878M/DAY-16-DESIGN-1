3cbe0800c12d1f4700e10fff6cd1d90809e60f87
KIRUBAHARAN M <105787201+KIRUBAHARAN8878M@users.noreply.github.com>
2022-07-26 22:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,105 @@
 var div5 = document.createElement("div");
 div5.setAttribute("class","calculator");
 
-div5.innerHTML=`<input type="text" placeholder="0" id="result">
-<button id ="clear" onclick="Clear()">C</button>
-<button id="delete" onclick="del()"><-</button>
-<button id="modu"onclick="display('%')">%</button>
-<button id="divi"onclick="display('/')">/</button>
-<button id="7"onclick="display('7')">7</button>
-<button id="8"onclick="display('8')">8</button>
-<button id="9"onclick="display('9')">9</button>
-<button id="multiple"onclick="display('*')">*</button>
-<button id="4"onclick="display('4')">4</button>
-<button id="5"onclick="display('5')">5</button>
-<button id="6"onclick="display('6')">6</button>
-<button id="subtract"onclick="display('-')">-</button>
-<button id="1"onclick="display('1')">1</button>
-<button id="2"onclick="display('2')">2</button>
-<button id="3"onclick="display('3')">3</button>
-<button id="add"onclick="display('+')">+</button>
-<button onclick="display('.')">.</button>
-<button id="0"onclick="display('0')">0</button>
-<button id="equal" onclick="Calculate()" class="equal">=</button>  `;
+div5.innerHTML=`<input type="text" placeholder="0" id="result">`;
+
+var button1 = document.createElement("button");
+button1.setAttribute("id","clear");
+button1.setAttribute("onclick","Clear()");
+button1.innerHTML='C';
+div5.append(button1);
+var button2 = document.createElement("button");
+button2.setAttribute("id","delete");
+button2.setAttribute("onclick","del()");
+button2.innerHTML="-";
+div5.append(button2);
+var button3 = document.createElement("button");
+button3.setAttribute("id","modu");
+button3.setAttribute("onclick","display('%')");
+button3.innerHTML="%";
+div5.append(button3);
+var button4 = document.createElement("button");
+button4.setAttribute("id","divi");
+button4.setAttribute("onclick","display('/')");
+button4.innerHTML="/";
+div5.append(button4);
+var button5 = document.createElement("button");
+button5.setAttribute("id","7");
+button5.setAttribute("onclick","display('7')");
+button5.innerHTML="7";
+div5.append(button5);
+var button6 = document.createElement("button");
+button6.setAttribute("id","8");
+button6.setAttribute("onclick","display('8')");
+button6.innerHTML="8";
+div5.append(button6);
+var button7 = document.createElement("button");
+button7.setAttribute("id","9");
+button7.setAttribute("onclick","display('9')");
+button7.innerHTML="9";
+div5.append(button7);
+var button8 = document.createElement("button");
+button8.setAttribute("id","multiple");
+button8.setAttribute("onclick","display('*')");
+button8.innerHTML="*";
+div5.append(button8);
+var button9 = document.createElement("button");
+button9.setAttribute("id","4");
+button9.setAttribute("onclick","display('4')");
+button9.innerHTML="4";
+div5.append(button9);
+var button10 = document.createElement("button");
+button10.setAttribute("id","5");
+button10.setAttribute("onclick","display('5')");
+button10.innerHTML="5";
+div5.append(button10);
+var button11 = document.createElement("button");
+button11.setAttribute("id","6");
+button11.setAttribute("onclick","display('6')");
+button11.innerHTML="6";
+div5.append(button11);
+var button12 = document.createElement("button");
+button12.setAttribute("id","subtract");
+button12.setAttribute("onclick","display('-')");
+button12.innerHTML="-";
+div5.append(button12);
+var button13 = document.createElement("button");
+button13.setAttribute("id","1");
+button13.setAttribute("onclick","display('1')");
+button13.innerHTML="1";
+div5.append(button13);
+var button14 = document.createElement("button");
+button14.setAttribute("id","2");
+button14.setAttribute("onclick","display('2')");
+button14.innerHTML="2";
+div5.append(button14);
+var button15 = document.createElement("button");
+button15.setAttribute("id","3");
+button15.setAttribute("onclick","display('3')");
+button15.innerHTML="3";
+div5.append(button15);
+var button16 = document.createElement("button");
+button16.setAttribute("id","add");
+button16.setAttribute("onclick","display('+')");
+button16.innerHTML="+";
+div5.append(button16);
+var button17 = document.createElement("button");
+button17.setAttribute("id","0");
+button17.setAttribute("onclick","display('.')");
+button17.innerHTML=".";
+div5.append(button17);
+var button18 = document.createElement("button");
+button18.setAttribute("id","a");
+button18.setAttribute("onclick","display('0')");
+button18.innerHTML="0";
+div5.append(button18);
+var button19 = document.createElement("button");
+button19.setAttribute("id","equal");
+button19.setAttribute("onclick","Calculate()");
+button19.setAttribute("class","equal")
+button19.innerHTML="=";
+div5.append(button19);
+
 document.getElementById("div3").append(div5);
 
 let output= document.getElementById("result");
